ce37dab08892edb0f3ae4c6323ec4e397f36ffa1
iDay <iDay@macbook-pro.lan>
2015-09-04 16:52:25 +0800

    原来watchPosition是定时去调用getCurrentPosition。。。。醉了。

--- www/Geolocation.js
+++ www/Geolocation.js
@@ -9,18 +9,29 @@ var ACTION_CLEAR_WATCH = 'clearWatch';
 
 var geolocation = module.exports = {};
 
+geolocation.watchId;
+
 geolocation.getCurrentPosition = function (success, error, options) {
   cordova.exec(success, error, SERVICE_NAME, ACTION_GET_CURRENT_POSITION, [options]);
 };
 
 geolocation.watchPosition = function (success, error, options) {
-  cordova.exec(success, error, SERVICE_NAME, ACTION_WATCH_POSITION, [options]);
+	if (typeof(geolocation.watchId) != "undefined") {
+		geolocation.clearWatch(geolocation.watchId);
+	}
+	geolocation.watchId = setInterval(function() {
+		geolocation.getCurrentPosition(success, error, options);
+	}, 10000);
+	return geolocation.watchId;
+//  cordova.exec(success, error, SERVICE_NAME, ACTION_WATCH_POSITION, [options]);
 };
 
 geolocation.clearWatch = function (watchId) {
-  cordova.exec(function () {
-    alert(JSON.stringify(arguments));
-  }, function () {
-    alert('fail');
-  }, SERVICE_NAME, ACTION_CLEAR_WATCH, [watchId]);
+	clearInterval(watchId);
+	geolocation.watchId = undefined;
+//  cordova.exec(function () {
+//    alert(JSON.stringify(arguments));
+//  }, function () {
+//    alert('fail');
+//  }, SERVICE_NAME, ACTION_CLEAR_WATCH, [watchId]);
 };
